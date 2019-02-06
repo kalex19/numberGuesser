d7001e2bc737ddfa9a64f8f7b1b4803e8b3e0f10
Brennan <brennanduffey@gmail.com>
2019-02-06 15:51:33 -0700

    min and max error messages

--- numberGuesser.js
+++ numberGuesser.js
@@ -1,29 +1,52 @@
-var min = document.querySelector(".min");
-var max = document.querySelector(".max");
-var minInput = document.querySelector(".min-input");
-var maxInput = document.querySelector(".max-input");
-var challengerNameOneInput = document.querySelector(".challenger-1-name-input");
-var challengerNameTwoInput = document.querySelector(".challenger-2-name-input");
-var challengerOneGuessInput = document.querySelector("#challenger-1-guess-input");
-var challengerTwoGuessInput = document.querySelector("#challenger-2-guess-input");
-var updateButton = document.querySelector(".update");
-var submitButton = document.querySelector(".submit-button");
+var min = document.querySelector('.min');
+var max = document.querySelector('.max');
+var minInput = document.querySelector('.min-input');
+var maxInput = document.querySelector('.max-input');
+var challengerNameOneInput = document.querySelector('.challenger-1-name-input');
+var challengerNameTwoInput = document.querySelector('.challenger-2-name-input');
+var challengerOneGuessInput = document.querySelector('#challenger-1-guess-input');
+var challengerTwoGuessInput = document.querySelector('#challenger-2-guess-input');
+var guessBox = document.querySelector('.guess-box')
+var updateButton = document.querySelector('.update');
+var submitButton = document.querySelector('.submit-button');
 var clearButton = document.querySelector('.clear-button');
 var resetButton = document.querySelector('.reset-button');
 var numGuesses = 0;
 var randomNumber;
 
+// challengerNameOneInput.value === '' && challengerNameTwoInput.value === '' && challengerOneGuessInput.value === '' && challengerTwoGuessInput.value === ''
 
-
-updateButton.addEventListener("click", updateRange);
-submitButton.addEventListener("click", submitInfo);
+updateButton.addEventListener('click', updateRange, disableReset);
+submitButton.addEventListener('click', submitInfo);
 clearButton.addEventListener('click', clearInput);
 resetButton.addEventListener('click', resetGame);
+guessBox.addEventListener('keyup', disableClear, disableReset);
+
+disableClear()
+disableReset()
+
+function disableReset() {
+  if (randomNumber !== undefined) {
+    resetButton.disabled = false;
+  } else {
+    resetButton.disabled = true;
+    generateRandomNumber()
+  }
+}
+
+function disableClear() {
+  if (challengerNameOneInput.value === '' && challengerNameTwoInput.value === '' && challengerOneGuessInput.value === '' && challengerTwoGuessInput.value === '') {
+    clearButton.disabled = true;
+  } else {
+    clearButton.disabled = false;
+  }
+}
 
 function updateRange() {  
   generateRandomNumber ();
   console.log(randomNumber);
   inputErrorMessages ();
+  disableReset()
 };
 
 
@@ -42,32 +65,31 @@ function nonAlphaNumericInput(event) {
   var regEx = /\W/;
   if (regEx.test(event.key)) {
     event.preventDefault();
-    // submitErrorMessage();
   }
 }
 
 function generateRandomNumber () {
-  min.innerText = minInput.value;
-  max.innerText = maxInput.value;
+  min.innerText = minInput.value || '1';
+  max.innerText = maxInput.value || '100';
   var minInteger = Math.ceil(parseInt(minInput.value,10));
   var maxInteger = Math.floor(parseInt(maxInput.value,10));
   randomNumber = Math.floor(Math.random() * (maxInteger - minInteger + 1)) + minInteger;
 };
 
 function inputErrorMessages () {
-  var minMoreMax = document.querySelector("#min-more-max")
-  var noRange = document.querySelector("#no-range");
+  var minMoreMax = document.querySelector('#min-more-max')
+  var noRange = document.querySelector('#no-range');
 
-  if (minInput.value === "" || maxInput.value === "") {
-    noRange.classList.remove("error-message");
+  if (minInput.value === '' || maxInput.value === '') {
+    noRange.classList.remove('error-message');
   } else {
     noRange.classList.add('error-message');
   }
 
   if (parseInt(maxInput.value) < parseInt(minInput.value)) {
-    minMoreMax.classList.remove("error-message");
+    minMoreMax.classList.remove('error-message');
   } else {
-    minMoreMax.classList.add("error-message");
+    minMoreMax.classList.add('error-message');
   }
 
 //DOES NOT WORK CORRECTLY
@@ -86,10 +108,10 @@ function inputErrorMessages () {
 };
 
 function latestScoreText() {
-  var challengerNameOne = document.querySelector(".challenger-1-name");
-  var challengerNameTwo = document.querySelector(".challenger-2-name");
-  var challengerOneGuess = document.querySelector(".challenger-1-guess");
-  var challengerTwoGuess = document.querySelector(".challenger-2-guess");
+  var challengerNameOne = document.querySelector('.challenger-1-name');
+  var challengerNameTwo = document.querySelector('.challenger-2-name');
+  var challengerOneGuess = document.querySelector('.challenger-1-guess');
+  var challengerTwoGuess = document.querySelector('.challenger-2-guess');
   challengerNameOne.innerText = challengerNameOneInput.value;
   challengerNameTwo.innerText = challengerNameTwoInput.value;
   challengerOneGuess.innerText = challengerOneGuessInput.value;
@@ -97,114 +119,121 @@ function latestScoreText() {
 };
 
 function playerFeedback(){
-  var challengerOneConditional = document.querySelector(".challenger-1-conditional");
-  var challengerTwoConditional = document.querySelector(".challenger-2-conditional");
-  var cardContainer = document.querySelector(".column-2");
+  var challengerOneConditional = document.querySelector('.challenger-1-conditional');
+  var challengerTwoConditional = document.querySelector('.challenger-2-conditional');
+  var cardContainer = document.querySelector('.column-2');
 
   if (parseInt(challengerOneGuessInput.value) > randomNumber) {
-    challengerOneConditional.innerText = "That's Too High"
+    challengerOneConditional.innerText = 'That\'s Too High'
   } else if (parseInt(challengerOneGuessInput.value) == randomNumber) {
-    challengerOneConditional.innerText = "BOOM"
+    challengerOneConditional.innerText = 'BOOM'
     pOneWinCard();    
   } else {
-    challengerOneConditional.innerText = "That's Too Low"
+    challengerOneConditional.innerText = 'That\'s Too Low'
   };
 
   if (parseInt(challengerTwoGuessInput.value) > randomNumber) {
-    challengerTwoConditional.innerText = "That's Too High";
+    challengerTwoConditional.innerText = 'That\'s Too High';
   } else if (parseInt(challengerTwoGuessInput.value) == randomNumber) {
-    challengerTwoConditional.innerText = "BOOM";
+    challengerTwoConditional.innerText = 'BOOM';
     pTwoWinCard();
   } else {
-    challengerTwoConditional.innerText = "That's Too Low"
+    challengerTwoConditional.innerText = 'That\'s Too Low'
   }
 };
 
-// function nonAlphaNumericInput(event){
-//   var regex = ; 
-// }
 
 function submitErrorMessages() {
   var errorMessageGuessOne = document.querySelector('.error-message-guess-one');
   var errorMessageGuessTwo = document.querySelector('.error-message-guess-two');
-  var noSubmit = document.querySelector(".no-submit");
+  var noSubmit = document.querySelectorAll('.no-submit');
+  console.log(noSubmit);
 
   if (parseInt(challengerOneGuessInput.value) < parseInt(minInput.value) || parseInt(challengerOneGuessInput.value) > parseInt(maxInput.value)) {
-    errorMessageGuessOne.classList.remove("error-message-guess-one")
+    errorMessageGuessOne.classList.remove('error-message-guess-one')
   } else {
-    errorMessageGuessOne.classList.add("error-message-guess-one")
+    errorMessageGuessOne.classList.add('error-message-guess-one')
   }
 
   if (parseInt(challengerTwoGuessInput.value) < parseInt(minInput.value) || parseInt(challengerTwoGuessInput.value) > parseInt(maxInput.value)) {
-    errorMessageGuessTwo.classList.remove("error-message-guess-two")
+    errorMessageGuessTwo.classList.remove('error-message-guess-two')
   } else {
-    errorMessageGuessTwo.classList.add("error-message-guess-two")
+    errorMessageGuessTwo.classList.add('error-message-guess-two')
   }
 
-//Need to change conditionals for error messages to properly display under correct name
-//I think adding an if statement for each error message for missing name input and missing guess input 
-//So basically will need an if statement for each input field message that will be displayed
-  if (challengerNameOneInput.value === ""){ 
-    noSubmit.classList.remove("error-message")
-  } else {
-    noSubmit.classList.add('error-message')
+  
+  for (var i = 0; i < noSubmit.length; i++){
+    if (noSubmit[i].value === ''){
+      console.log(noSubmit[i].parentElement.nextElementSibling);
+      noSubmit[i].parentElement.nextElementSibling.classList.remove('error-message');
+    } else {
+      noSubmit[i].parentElement.nextElementSibling.classList.add('error-message')
+    }
   }
+}
 
- if (challengerGuessOneInput.value === ""){ 
-    noSubmit.classList.remove("error-message")
-  } else {
-    noSubmit.classList.add('error-message')
-  }
 
- if (challengerNameTwoInput.value === ""){ 
-    noSubmit.classList.remove("error-message")
-  } else {
-    noSubmit.classList.add('error-message')
-  }
+ //  if (challengerNameOneInput.value === ''){ 
+ //    noSubmit.classList.remove('error-message')
+ //  } else {
+ //    noSubmit.classList.add('error-message')
+ //  }
 
- if (challengerGuessOneInput.value === ""){ 
-    noSubmit.classList.remove("error-message")
-  } else {
-    noSubmit.classList.add('error-message')
-  }
-};
+ // if (challengerOneGuessInput.value === ''){ 
+ //    noSubmit.classList.remove('error-message')
+ //  } else {
+ //    noSubmit.classList.add('error-message')
+ //  }
+
+ // if (challengerNameTwoInput.value === ''){ 
+ //    noSubmit.classList.remove('error-message')
+ //  } else {
+ //    noSubmit.classList.add('error-message')
+ //  }
+
+ // if (challengerTwoGuessInput.value === ''){ 
+ //    noSubmit.classList.remove('error-message')
+ //  } else {
+ //    noSubmit.classList.add('error-message')
+ //  }
 
 
 function clearInput() {
-  challengerNameOneInput.value = "";
-  challengerNameTwoInput.value = "";
-  challengerOneGuessInput.value = "";
-  challengerTwoGuessInput.value = "";
+  challengerNameOneInput.value = '';
+  challengerNameTwoInput.value = '';
+  challengerOneGuessInput.value = '';
+  challengerTwoGuessInput.value = '';
+  disableClear();
 };
 
 function resetGame() {
   numGuesses = 0;
-  challengerNameOneInput.value = "";
-  challengerNameTwoInput.value = "";
-  challengerOneGuessInput.value = "";
-  challengerTwoGuessInput.value = "";
-  randomNumber = Math.floor(Math.random() * (parseInt(maxInput.value) - parseInt(minInput.value) + 1)) + parseInt(minInput.value);
+  challengerNameOneInput.value = '';
+  challengerNameTwoInput.value = '';
+  challengerOneGuessInput.value = '';
+  challengerTwoGuessInput.value = '';
+  generateRandomNumber();
 };
 
 function pOneWinCard() {
   cardContainer.innerHTML += `
   <article>
-    <div class="card">
+    <div class='card'>
       <h4>${challengerNameOneInput.value}</h4>
       <p>vs</p>
       <h4>${challengerNameTwoInput.value}</h4>
     </div>
     <hr />
-    <div class="middle-row">
-      <h2 class="winner"><strong>${challengerNameOneInput.value}</strong></h2> 
+    <div class='middle-row'>
+      <h2 class='winner'><strong>${challengerNameOneInput.value}</strong></h2> 
       <br />
       <p class='winner2'>WINNER</p>
     </div>
     <hr />
-    <div class="card">
+    <div class='card'>
       <p><strong>#</strong> ${numGuesses}</p>
       <p><strong>00</strong>MINUTES}</p>
-      <button class="exit">X</button>
+      <button class='exit'>X</button>
     </div>
   </article>`
   numGuesses = 0;
@@ -214,22 +243,22 @@ function pOneWinCard() {
 function pTwoWinCard() {
   cardContainer.innerHTML += `
   <article>
-    <div class="card">
+    <div class='card'>
       <h4>${challengerNameOneInput.value}</h4>
       <p>vs</p>
       <h4>${challengerNameTwoInput.value}</h4>
     </div>
     <hr />
-    <div class="middle-row">
-      <h2 class="winner"><strong>${challengerNameTwoInput.value}</strong></h2> 
+    <div class='middle-row'>
+      <h2 class='winner'><strong>${challengerNameTwoInput.value}</strong></h2> 
       <br />
       <p class='winner2'>WINNER</p>
     </div>
     <hr />
-    <div class="card">
+    <div class='card'>
       <p><strong>#</strong> ${numGuesses}</p>
       <p><strong>00</strong>MINUTES}</p>
-      <button class="exit">X</button>
+      <button class='exit'>X</button>
     </div>
   </article>`
   numGuesses = 0;
